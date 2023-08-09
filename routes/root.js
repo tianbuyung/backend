47a6fbe93dafeefbbd271f956f8c803a2f26ae40
Septian Maulana <septianm028@gmail.com>
2023-08-09 13:36:23 +0700

    build: set up project structure and basic scaffolding

---
+++ routes/root.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
+router.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "new-page.html"));
+});
+
+router.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html"); // by default redirect to new-page.html with status code 302
+});
+
+router.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+
+const three = (req, res) => {
+  console.log("three");
+  res.send("Finished!");
+};
+
+router.get("/chain(.html)?", [one, two, three]);
+
+module.exports = router;
