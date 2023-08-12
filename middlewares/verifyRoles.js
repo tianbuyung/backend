a7c7607ccb885923ed7d7cd8db9e153e3482020b
Septian Maulana <septianm028@gmail.com>
2023-08-12 16:01:45 +0700

    chore: delete unnecessary console log

--- middlewares/verifyRoles.js
+++ middlewares/verifyRoles.js
@@ -3,13 +3,10 @@ const verifyRoles = (...allowedRoles) => {
     if (!req?.roles) return res.sendStatus(401);
 
     const rolesArray = [...allowedRoles];
-    console.log(rolesArray);
-    console.log(req.roles);
 
     const result = req.roles
       .map((role) => rolesArray.includes(role))
       .find((val) => val === true);
-    console.log(result);
 
     if (!result) return res.sendStatus(401);
 
