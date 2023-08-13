f1b3b8f72622f741206d4df2eba81e8ff4c93d4d
Septian Maulana <septianm028@gmail.com>
2023-08-14 01:22:48 +0700

    fix: typing error for import models

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,4 +1,4 @@
-const Employee = require("../models/employee");
+const Employee = require("../models/Employee");
 
 const readAllEmployees = async (req, res) => {
   const employees = await Employee.find();
--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 
 const handleLogout = async (req, res) => {
   // On client also delete accessToken
