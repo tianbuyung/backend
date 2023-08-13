71b0071a1049ae05571ad2f1edbc2cc588b438ed
Septian Maulana <septianm028@gmail.com>
2023-08-13 21:06:26 +0700

    chore: update auth, logout and refresh token with mongodb function; add timestamp into mongodb schemes

--- models/User.js
+++ models/User.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  roles: {
-    User: {
-      type: Number,
-      default: 2001,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    Editor: Number,
-    Admin: Number,
+    roles: {
+      User: {
+        type: Number,
+        default: 2001,
+      },
+      Editor: Number,
+      Admin: Number,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: String,
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  refreshToken: String,
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
